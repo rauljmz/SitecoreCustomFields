1754b67ff7f77e723c170b99d81b035d0af9b14e
Raul Jimenez <rj@sitecore.net>
2014-03-06 17:42:00 +0000

    initial commit

---
+++ CustomFields/Controls/TreeList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Shell.Applications.ContentEditor;
+
+namespace CustomFields.Controls
+{
+    public class CustomTreeList : TreeList
+    {
+        private static string QueryPrefix = "query:";
+        public override string DataSource
+        {
+            get
+            {
+                return base.DataSource;
+            }
+            set
+            {
+                base.DataSource = CheckForQuery(value);
+            }
+        }
+        protected virtual string CheckForQuery(string value)
+        {
+            if (value.StartsWith(QueryPrefix))
+            {
+                var item = Sitecore.Context.ContentDatabase.GetItem(this.ItemID);
+                if (item != null)
+                {
+                    var root = item.Axes.SelectSingleItem(value.Substring(QueryPrefix.Length));
+                    if (root != null)
+                    {
+                        return root.ID.ToString();
+                    }
+                }
+            }
+            return value;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+CustomFields
+=================
+
+This plugin adds functionality to some of the existing Sitecore fields.
+
+###TreeList
+
+Now supports the use of a query to specify the root of the tree. It can be absolute or relative:
+
+e.g. datasource=query:./ancestor-or-self::*[@@templatekey='sample item']
